4b296b2c68021b070c1aa6eece2b9838b704d82d
Shivam <shivambhati290@gmail.com>
2025-08-07 21:51:44 +0530

    pred

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 // const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  output: 'export', // Required for `next export`
-  distDir: 'dist',
+  // output: 'export', // Required for `next export`
+  // distDir: 'dist',
   images:{
     unoptimized:true,
   }
